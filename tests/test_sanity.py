782e8208c18d88125146082bdbc3e1df046b394b
amon <amon@nandynarwhals.org>
2018-09-30 16:59:50 +0800

    Initial commit

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -4,10 +4,10 @@
 """This module provides a single sanity test to demonstrate the import of the module is successful.
 """
 
-import python_stub.version
+import basic_mondrian.version
 
 
 def test_project_version_exists():
     """Tests that the type of the version attribute of the package is string.
     """
-    assert type(python_stub.version.__version__) is str
+    assert type(basic_mondrian.version.__version__) is str
